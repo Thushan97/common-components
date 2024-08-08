680ecaf94e1aa0077b7145c5887f1e1f601578fe
Thushan De Silva <thushan@fcodelabs.com>
2024-08-09 00:02:14 +0530

    Checking with border radius

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { styled as materialStyled } from "@mui/material/styles";
 
-const CustomButton = () => <div>test</div>;
+const CustomButton = materialStyled(LoadingButton)(({ theme }) => ({
+  borderRadius: "50px",
+  padding: "12px",
+  textAlign: "center",
+  textTransform: "none",
+  fontFamily: "Satoshi",
+  fontSize: 16,
+  fontStyle: "normal",
+  fontWeight: 500,
+  lineHeight: "normal",
+  height: "38px",
+  "&:disabled": {
+    backgroundColor: theme.palette.action.disabledBackground, // Disabled button color
+    color: theme.palette.white.main, // Disabled text color
+  },
+}));
 
 export default CustomButton;
