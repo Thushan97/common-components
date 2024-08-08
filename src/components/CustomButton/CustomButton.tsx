1137a21301bd92ce6b07d88ee70313b09ce0d7bc
Thushan De Silva <thushan@fcodelabs.com>
2024-08-08 23:57:33 +0530

    Add test

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,21 +1,5 @@
-import LoadingButton from "@mui/lab/LoadingButton";
-import { styled as materialStyled } from "@mui/material/styles";
+import React from "react";
 
-const CustomButton = materialStyled(LoadingButton)(({ theme }) => ({
-  borderRadius: "50px",
-  padding: "100px",
-  textAlign: "center",
-  textTransform: "none",
-  fontFamily: "Satoshi",
-  fontSize: 16,
-  fontStyle: "normal",
-  fontWeight: 500,
-  lineHeight: "normal",
-  height: "50px",
-  "&:disabled": {
-    backgroundColor: theme.palette.action.disabledBackground, // Disabled button color
-    color: theme.palette.white.main, // Disabled text color
-  },
-}));
+const CustomButton = () => <div>test</div>;
 
 export default CustomButton;
