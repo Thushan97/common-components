22bdd6b337fd7b6204fd4d1b0f1b7a236c118f18
Thushan De Silva <thushan@fcodelabs.com>
2024-08-09 10:21:26 +0530

    Add custom menu

---
+++ src/components/CustomMenuItem/CustomMenuItem.tsx
@@ -0,0 +1,8 @@
+import { MenuItem, styled } from '@mui/material';
+
+const CustomMenuItem = styled(MenuItem)(() => ({
+  padding: '16px',
+  width: '194px !important',
+}));
+
+export default CustomMenuItem;
