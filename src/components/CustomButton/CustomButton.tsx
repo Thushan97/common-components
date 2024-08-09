cbfd3b1897fabf5754c9812c77b5a6a94d96a235
Thushan De Silva <thushan@fcodelabs.com>
2024-08-09 09:30:33 +0530

    Add props

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,21 +1,30 @@
 import LoadingButton from "@mui/lab/LoadingButton";
 import { styled as materialStyled } from "@mui/material/styles";
+import { theme } from "../../theme/theme";
 
-const CustomButton = materialStyled(LoadingButton)(({ theme }) => ({
-  borderRadius: "50px",
-  padding: "12px",
-  textAlign: "center",
-  textTransform: "none",
-  fontFamily: "Satoshi",
-  fontSize: 16,
-  fontStyle: "normal",
-  fontWeight: 500,
-  lineHeight: "normal",
-  height: "38px",
-  "&:disabled": {
-    backgroundColor: theme.palette.action.disabledBackground, // Disabled button color
-    color: theme.palette.white.main, // Disabled text color
-  },
-}));
+type ICustomButtonProps = {
+  padding?: string;
+  radius?: string;
+  height?: string;
+};
+
+const CustomButton = materialStyled(LoadingButton)<ICustomButtonProps>(
+  ({ padding = "12px", radius = "4px", height = "38px" }) => ({
+    borderRadius: radius,
+    padding,
+    textAlign: "center",
+    textTransform: "none",
+    fontFamily: "Satoshi",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "normal",
+    height,
+    "&:disabled": {
+      backgroundColor: theme.palette.action.disabledBackground, // Disabled button color
+      color: theme.palette.white.main, // Disabled text color
+    },
+  })
+);
 
 export default CustomButton;
