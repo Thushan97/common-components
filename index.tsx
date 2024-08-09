bf8f829e567d61fb1cef05c7a08bcdb35c6ee691
Thushan De Silva <thushan@fcodelabs.com>
2024-08-10 00:03:59 +0530

    test

--- index.tsx
+++ index.tsx
@@ -1,25 +1,33 @@
-import ConfirmationMessage from "./src/components/ConfirmationMessage/ConfirmationMessage";
+import ConfirmationMessage, {
+  IConfirmationMessageProps,
+} from "./src/components/ConfirmationMessage/ConfirmationMessage";
 import CustomButton from "./src/components/CustomButton/CustomButton";
 import CustomContainerBox from "./src/components/CustomContainerBox/CustomContainerBox";
 import CustomMenuItem from "./src/components/CustomMenuItem/CustomMenuItem";
-import CustomPasswordField from "./src/components/CustomPasswordField/CustomPasswordField";
+import CustomPasswordField, {
+  ICustomPasswordFieldProps,
+} from "./src/components/CustomPasswordField/CustomPasswordField";
 import CustomTextField from "./src/components/CustomTextField/CustomTextField";
 import ToggleButtons from "./src/components/ToggleButton/ToggleButton";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./src/theme/theme";
 import React from "react";
 
-const withThemeProvider = (Component: React.ComponentType) => (props: any) =>
-  (
-    <ThemeProvider theme={theme}>
-      <Component {...props} />
-    </ThemeProvider>
-  );
+const withThemeProvider =
+  <P extends object>(Component: React.ComponentType<P>) =>
+  (props: P) =>
+    (
+      <ThemeProvider theme={theme}>
+        <Component {...props} />
+      </ThemeProvider>
+    );
 
-// export const ThemedConfirmationMessage = withThemeProvider(ConfirmationMessage);
-export const ThemedCustomButton = withThemeProvider(CustomButton);
-export const ThemedCustomContainerBox = withThemeProvider(CustomContainerBox);
-export const ThemedCustomMenuItem = withThemeProvider(CustomMenuItem);
-// export const ThemedCustomPasswordField = withThemeProvider(CustomPasswordField);
-export const ThemedCustomTextField = withThemeProvider(CustomTextField);
-export const ThemedToggleButtons = withThemeProvider(ToggleButtons);
+export const CommonConfirmationMessage =
+  withThemeProvider<IConfirmationMessageProps>(ConfirmationMessage);
+export const CommonButton = withThemeProvider(CustomButton);
+export const CommonContainerBox = withThemeProvider(CustomContainerBox);
+export const CommonMenuItem = withThemeProvider(CustomMenuItem);
+export const CommonPasswordField =
+  withThemeProvider<ICustomPasswordFieldProps>(CustomPasswordField);
+export const CommonTextField = withThemeProvider(CustomTextField);
+export const CommonToggleButtons = withThemeProvider(ToggleButtons);
