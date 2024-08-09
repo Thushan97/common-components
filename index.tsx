6901331e5648eaeb09270f90450ff368f1cefd92
Thushan De Silva <thushan@fcodelabs.com>
2024-08-09 23:07:28 +0530

    test

--- index.tsx
+++ index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ConfirmationMessage from "./src/components/ConfirmationMessage/ConfirmationMessage";
 import CustomButton from "./src/components/CustomButton/CustomButton";
 import CustomContainerBox from "./src/components/CustomContainerBox/CustomContainerBox";
@@ -6,20 +5,13 @@ import CustomMenuItem from "./src/components/CustomMenuItem/CustomMenuItem";
 import CustomPasswordField from "./src/components/CustomPasswordField/CustomPasswordField";
 import CustomTextField from "./src/components/CustomTextField/CustomTextField";
 import ToggleButtons from "./src/components/ToggleButton/ToggleButton";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./src/theme/theme";
 
-const withThemeProvider = (Component: React.ComponentType) => (props: any) =>
-  (
-    <ThemeProvider theme={theme}>
-      <Component {...props} />
-    </ThemeProvider>
-  );
-
-// export const ThemedConfirmationMessage = withThemeProvider(ConfirmationMessage);
-export const CommonButton = withThemeProvider(CustomButton);
-export const CommonContainerBox = withThemeProvider(CustomContainerBox);
-export const CommonMenuItem = withThemeProvider(CustomMenuItem);
-// export const CommonPasswordField = withThemeProvider(CustomPasswordField);
-export const CommonTextField = withThemeProvider(CustomTextField);
-export const commonToggleButtons = withThemeProvider(ToggleButtons);
+export {
+  ConfirmationMessage,
+  CustomButton,
+  CustomContainerBox,
+  CustomMenuItem,
+  CustomPasswordField,
+  CustomTextField,
+  ToggleButtons,
+};
